0208201f6173d4eacc2d0eec7d80a4c029fcfa45
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-29 02:47:15 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -30,6 +30,9 @@ async def remove_admin(client, message):
         await message.reply_text("Please provide the user's ID to remove them as an admin.")
 
   
+def is_maintenance_mode_enabled():
+    return Config.MAINTENANCE_MODE
+
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
     user = message.from_user
