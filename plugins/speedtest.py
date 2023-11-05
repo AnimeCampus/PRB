8b53aec033741dd6d53e920c91b4d7da762679b7
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-11-05 09:47:02 +0530

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -1,4 +1,5 @@
 import os
+import speedtest
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
@@ -7,23 +8,17 @@ async def run_speedtest(client: Client, message: Message):
     m = await message.reply_text("⚡️ Running Speedtest")
 
     try:
-        # Run the fast-cli command to check internet speed
-        speedtest_result = os.popen("fast").read()
+        # Create a SpeedtestClient object
+        st = speedtest.Speedtest()
 
-        # Extract download and upload speeds from the result
-        download_speed = None
-        upload_speed = None
+        # Get the best server for testing
+        st.get_best_server()
 
-        for line in speedtest_result.split('\n'):
-            if "Download" in line:
-                download_speed = line.strip()
-            elif "Upload" in line:
-                upload_speed = line.strip()
+        # Run the download and upload speed tests
+        download_speed = st.download() / 1024 / 1024  # Convert to Mbps
+        upload_speed = st.upload() / 1024 / 1024  # Convert to Mbps
 
-        if download_speed and upload_speed:
-            result_text = f"🚀 Download Speed: {download_speed}\n🚀 Upload Speed: {upload_speed}"
-        else:
-            result_text = "Speed test result format is not recognized."
+        result_text = f"🚀 Download Speed: {download_speed:.2f} Mbps\n🚀 Upload Speed: {upload_speed:.2f} Mbps"
 
         # Send the results as text
         await m.edit(result_text)
