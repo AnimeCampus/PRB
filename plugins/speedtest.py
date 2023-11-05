58d6ea51586bc6b48f44f17f159f31d2bc6fc7ff
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-11-05 09:49:50 +0530

    Update speedtest.py

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -1,26 +1,29 @@
 import os
-import speedtest
+import httpx
 from pyrogram import Client, filters
 from pyrogram.types import Message
+import json
 
 @Client.on_message(filters.command("speedtest"))
 async def run_speedtest(client: Client, message: Message):
     m = await message.reply_text("⚡️ Running Speedtest")
 
     try:
-        # Create a SpeedtestClient object
-        st = speedtest.Speedtest()
+        async with httpx.AsyncClient() as client:
+            response = await client.get("https://www.speedtest.net/api/js/speedtest-config.php")
 
-        # Get the best server for testing
-        st.get_best_server()
+        if response.status_code == 200:
+            config_data = json.loads(response.text)
 
-        # Run the download and upload speed tests
-        download_speed = st.download() / 1024 / 1024  # Convert to Mbps
-        upload_speed = st.upload() / 1024 / 1024  # Convert to Mbps
+            download_speed = config_data["download"]["bandwidth"] / 1024 / 1024  # Convert to Mbps
+            upload_speed = config_data["upload"]["bandwidth"] / 1024 / 1024  # Convert to Mbps
 
-        result_text = f"🚀 Download Speed: {download_speed:.2f} Mbps\n🚀 Upload Speed: {upload_speed:.2f} Mbps"
+            result_text = f"🚀 Download Speed: {download_speed:.2f} Mbps\n🚀 Upload Speed: {upload_speed:.2f} Mbps"
 
-        # Send the results as text
-        await m.edit(result_text)
+            # Send the results as text
+            await m.edit(result_text)
+        else:
+            await m.edit("Failed to fetch speed test data.")
     except Exception as e:
         await m.edit(str(e))
+
