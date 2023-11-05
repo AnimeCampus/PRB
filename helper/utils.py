e8239c5819df72a7c67b9fb9c5e72ba55d7a1c9f
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-11-05 13:59:04 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -8,31 +8,31 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 5.00) == 0 or current == total:        
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
+    percentage = current * 100 / total
+    speed = current / diff
+    elapsed_time = round(diff) * 1000
+
+    if round(percentage) % 10 == 0 or current == total:
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
 
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "{0}{1}".format(
-            ''.join(["▓" for i in range(math.floor(percentage / 5))]),
-            ''.join(["▒" for i in range(20 - math.floor(percentage / 5))])
-        )            
-        tmp = progress + Txt.PROGRESS_BAR.format( 
-            round(percentage, 2),
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),            
-            estimated_total_time if estimated_total_time != '' else "0 s"
+        progress = "".join(["▓" for _ in range(math.floor(percentage / 5))]) + "".join(["▒" for _ in range(20 - math.floor(percentage / 5)])
+        progress_text = (
+            f"{ud_type}\n\n"
+            f"{progress} {round(percentage, 2)}%\n"
+            f"Completed: {humanbytes(current)} / {humanbytes(total)}\n"
+            f"Speed: {humanbytes(speed)}/s\n"
+            f"Elapsed Time: {elapsed_time}\n"
+            f"Estimated Time: {estimated_total_time if estimated_total_time else '0 s'}"
         )
+
         try:
             await message.edit(
-                text=f"{ud_type}\n\n{tmp}",               
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])                                               
+                text=progress_text,
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])
             )
         except:
             pass
